3f44f7f667089661d8992459f32a22ed7f35e4ad
George Ikigu <ikigu.geo@gmail.com>
2024-08-01 19:37:39 +0300

    solution: task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Has floor function"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Turns a float into an int
+
+    Args:
+        n (float): a float
+
+    Returns:
+        the floor of n
+    """
+
+    return math.floor(n)

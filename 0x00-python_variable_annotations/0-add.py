81f788f9c53a36dff0e2a666a4ca28fbcc64f990
George Ikigu <ikigu.geo@gmail.com>
2024-08-01 19:21:27 +0300

    solution: task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Contains a function that adds"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds a and b
+
+    Args:
+        a (float): a float
+        b (float): another float
+
+    Returns:
+        sum of a and b
+    """
+
+    return a + b

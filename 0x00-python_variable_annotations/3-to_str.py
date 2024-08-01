e02de3ac8cc16ffe88f9bb3a7d20a3621332b001
George Ikigu <ikigu.geo@gmail.com>
2024-08-01 19:40:05 +0300

    solution: task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Has to_str function"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float and returns the string representation of that float
+
+    Args:
+        n (float): a float
+
+    Return:
+        String representation of n
+    """
+
+    return str(n)

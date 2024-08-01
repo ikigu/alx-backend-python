9797fff2d39510ba322b264ee232c95c10d4a63c
George Ikigu <ikigu.geo@gmail.com>
2024-08-01 19:28:47 +0300

    docs: add function documenation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,4 +4,13 @@
 
 
 def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings
+
+    Args:
+        str1 (string): a string
+        str2 (string): another string
+
+    Return:
+        Concatenations of str1 and str2
+    """
     return str1 + str2

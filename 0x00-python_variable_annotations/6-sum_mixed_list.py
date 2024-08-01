904c0efd5fc87e9194778a2c9c20236e3113cdd3
George Ikigu <ikigu.geo@gmail.com>
2024-08-01 20:04:01 +0300

    solution: task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Has sum_mixed_list function"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, int]]) -> float:
+    """
+    Gets sum of a mixed list
+    """
+
+    sum: float = 0.0
+
+    for number in mxd_lst:
+        sum = sum + number
+
+    return sum
